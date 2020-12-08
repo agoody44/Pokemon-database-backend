77b476aedfe94ca97c2dc3306872497d3789c77a
“Alyssa <agoody44@gmail.com>
2020-12-08 13:25:23 -0500

    adds check auth helper to scores router

--- routes/api/scores.js
+++ routes/api/scores.js
@@ -8,9 +8,10 @@ router.get('/', scoresCtrl.highScores);
 // Process the token for only the routes below
 router.use(require('../../config/auth'));
 // Adds checkAuth so if a user is NOT signed in or token is not valid, gives 'Not Authorized' error.
+// if user is valid, allows them to hit the route.
 router.post('/', checkAuth, scoresCtrl.create);
 
-/*----- Restricts access so ONLY vailid users can access -----*/
+/*----- Restricts access so ONLY valid users can access -----*/
 function checkAuth(req, res, next) {
     if (req.user) return next();
     return res.status(401).json({ msg: 'Not Authorized' })
